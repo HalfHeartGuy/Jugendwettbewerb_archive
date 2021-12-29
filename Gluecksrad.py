ca959d5cabc9ff3b9b52f03cbb369531d744f3fc
HalfHeartGuy <richard.h.guo@gmail.com>
2021-12-29 18:03:05 +0100

    step 3 Gluecksrad 30. Bundesinformatik Junioraufgaben2

--- Gluecksrad.py
+++ Gluecksrad.py
@@ -6,7 +6,11 @@
 import math
 possibilityTable = {"1":5/15,"2":4/15,"3":3/15,"4":2/15,"5":1/15}
 
-lettterSquence = "A-C-D-B-E-A-F"
+lettterSquence = "A-B-C-A-F-E"
+lettterSquence1 = "A-D-A-D-A-D-A"
+possibilityTable1 = {"1":4/15,"2":3/15,"3":2/15,"4":2/15,"5":2/15,"6":1/15,"7":1/15}
+
+
 def caculatePossibility(myPossibilityDic,myletterSquence):
     myResult = 0
     step1_result = step1(myletterSquence)
@@ -18,6 +22,8 @@ def caculatePossibility(myPossibilityDic,myletterSquence):
     step3_result = step3(myPossibilityDic,step2_result)
     print(step3_result)
 
+    step4_result = step4(step3_result)
+    print(step4_result)
 def step1(myletterSquence):
     return myletterSquence.split("-")
 
@@ -35,8 +41,22 @@ def step3(myPossibilityDic,step2Results):
         wahrscheinlichkeit = myPossibilityDic[str(step2Results[i])]
         results.append(wahrscheinlichkeit)
     return results
+def step4(step3Result):
+    result = 1
+    for i in step3Result:
+        result = result * i
+    return result
 
 
 
 
-caculatePossibility(possibilityTable,lettterSquence)
+def caculator(mypossibilitydic,letterSquence):
+    caculatePossibility(mypossibilitydic, lettterSquence)
+    return letterSquence
+
+
+caulator = caculator(possibilityTable,lettterSquence1)
+caulator = caculator(possibilityTable1,lettterSquence1)
+
+
+
