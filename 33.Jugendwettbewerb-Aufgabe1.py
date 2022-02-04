57917008a32095891d9e954f3cb149e15929d783
HalfHeartGuy <richard.h.guo@gmail.com>
2022-02-04 19:22:08 +0100

    32. Jugendwettbewerb Junioraufabe 1

--- 33.Jugendwettbewerb-Aufgabe1.py
+++ 33.Jugendwettbewerb-Aufgabe1.py
@@ -1,5 +1,4 @@
 warteschlange = [4.14, 3.63, 3.92, 7.95, 5.23, 3.30, 4.86, 15.06]
-faehre = []
 auffahrt1 = []
 auffahrt2 = []
 auffahrt3 = []
@@ -7,29 +6,39 @@ uebrigerplatz1 = 20.0
 uebrigerplatz2 = 20.0
 uebrigerplatz3 = 20.0
 def fährebegleiter(warteschlange,uebrigerplatz1,uebrigerplatz2,uebrigerplatz3):
-
+    faehre = []
+    print(warteschlange)
     for onecar in warteschlange:
         if onecar + 0.3 < float(uebrigerplatz1):
             auffahrt1.append(onecar)
-            uebrigerplatz1 = onecar + 0.3
+            autoplatz1 = onecar + 0.3
+            uebrigerplatz1 = uebrigerplatz1 - autoplatz1
+            print("parkbahn1" + str(auffahrt1))
         else:
             if onecar + 0.3 < float(uebrigerplatz2):
                 auffahrt2.append(onecar)
-                uebrigerplatz2 = onecar + 0.3
+                autoplatz2 = onecar + 0.3
+                uebrigerplatz2 = uebrigerplatz2 - autoplatz2
+                print("parkbahn2" + str(auffahrt2))
 
             else:
                 if onecar + 0.3 < float(uebrigerplatz3):
                     auffahrt3.append(onecar)
-                    uebrigerplatz3 = onecar + 0.3
+                    autoplatz3 = onecar + 0.3
+                    uebrigerplatz3 = uebrigerplatz3 - autoplatz3
+                    print("parkbahn3" + str(auffahrt3))
 
-                else:
-                    faehre.append(auffahrt1)
-                    faehre.append(auffahrt2)
-                    faehre.append(auffahrt3)
-                    return faehre
+
+
+    faehre.append(auffahrt1)
+    faehre.append(auffahrt2)
+    faehre.append(auffahrt3)
+    return faehre
 
 
 print(fährebegleiter(warteschlange,uebrigerplatz1,uebrigerplatz2,uebrigerplatz3))
+
+
 
 
 
