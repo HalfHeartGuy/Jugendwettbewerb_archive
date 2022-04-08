bf5d1a1a733d8a3687fd95df07375ced47a6f162
HalfHeartGuy <richard.h.guo@gmail.com>
2022-04-08 17:33:27 +0200

    Berechnung von Kürzen,neue system von eingabe

--- Aufgaben_JWB34.py
+++ Aufgaben_JWB34.py
@@ -18,16 +18,57 @@ def hatUeberschneidung(rechteck1,rechteck2):
     rechteck1_groesster_y = rechteck1[3]
     rechteck1_kleinster_x = rechteck1[0]
     rechteck1_kleinster_y = rechteck1[1]
-    if rechteck2[3] < rechteck1_groesster_y and rechteck2[3] > rechteck1_kleinster_y:
-        if rechteck2[2] < rechteck1_groesster_x and rechteck2[2] > rechteck1_kleinster_x:
+    rechteck2Y2 = rechteck2[3]
+    Rechteck2X1 = rechteck2[0]
+    rechteck2Y1 = rechteck2[1]
+    rechteck2X2 = rechteck2[2]
+
+    return (rechteck2Y2 < rechteck1_groesster_y and rechteck2Y2 > rechteck1_kleinster_y and rechteck2X2 < rechteck1_groesster_x and rechteck2X2 > rechteck1_kleinster_x) \
+            or(Rechteck2X1 < rechteck1_groesster_x and Rechteck2X1 > rechteck1_kleinster_x and rechteck2Y1 < rechteck1_groesster_y and rechteck2Y1 > rechteck1_kleinster_y) \
+            or (Rechteck2X1 < rechteck1_groesster_x and Rechteck2X1 > rechteck1_kleinster_x and rechteck2Y2 < rechteck1_groesster_y and rechteck2Y2 > rechteck1_kleinster_y) \
+            or (rechteck2Y2 < rechteck1_groesster_x and rechteck2X2 > rechteck1_kleinster_x and Rechteck2X1 < rechteck1_groesster_y and rechteck2Y1 > rechteck1_kleinster_y)\
+            or (rechteck2Y2 > rechteck1_groesster_y and rechteck2Y1 < rechteck1_kleinster_y) and rechteck2X2 < rechteck2X2 and rechteck2X2 > rechteck1_kleinster_x\
+            or (rechteck2Y2 > rechteck1_groesster_x and rechteck2Y1 < rechteck1_kleinster_y and Rechteck2X1 < rechteck1_groesster_x and Rechteck2X1 > rechteck1_kleinster_x)\
+            or (rechteck2X2 > rechteck1_groesster_x and Rechteck2X1 < rechteck1_kleinster_y and rechteck2Y2 < rechteck1_groesster_y and rechteck2Y2 > rechteck1_kleinster_y)\
+#Fall1:Oben rechts Ecke vom rechteck1 ist innerhalb rechteck2
+#Fall2:Unten linke ecke vom rechteck1 ist innerhalb rechteck2
+#Fall3:Oben links Ecke von Rechteck2 ist innerhalb Rechteck1
+#Fall4:Unten Rechts Ecke von Rechteck2 ist innerhalb Rechteck1
+#Fall 5:Beide Ecken von Rechteck 2 sind außerhalb vom rechteck1.
+#Fall6:Beide Ecken von Rechteck 2 sind außerhalb rechteck1.
+def berecheneLandnahme(kordinatenliste):
+    dictionary = {}
+    for onerechteck in kordinatenliste:
+
+        dictionary[onerechteck]=True
+
+        for vierecke in dictionary.keys():
+
+            Status = hatUeberschneidung(vierecke,onerechteck)
+            if Status:
+                dictionary[onerechteck]=False
+                break
+
+    print(dictionary)
+
+
+
+
+berecheneLandnahme([(2,3,5,5),(1,2,4,4),(3,1,6,3)])
+
+"""
+    if rechteck2Y2 < rechteck1_groesster_y and rechteck2Y2 > rechteck1_kleinster_y:
+        rechteck2X2 = rechteck2[2]
+        if rechteck2X2 < rechteck1_groesster_x and rechteck2X2 > rechteck1_kleinster_x:
 
             return False
-    elif rechteck2[0] < rechteck1_groesster_x and rechteck2[0] > rechteck1_kleinster_x:
-        if rechteck2[1] < rechteck1_groesster_y and rechteck2[1] > rechteck1_kleinster_y:
+    elif Rechteck2X1 < rechteck1_groesster_x and Rechteck2X1 > rechteck1_kleinster_x:
+        rechteck2Y1 = rechteck2[1]
+        if rechteck2Y1 < rechteck1_groesster_y and rechteck2Y1 > rechteck1_kleinster_y:
             return False
     else:
         return True
-
+"""
 #Schritt 2:vergleich ein Rechtecken mit vorherigen(mit Statusspeicherung)
 #[(1,2,3,5):True, (3,2,1,5):False, ....,]
 
@@ -45,4 +86,11 @@ for eachRechteck in list:
 
 
 dict2 = {"key1":True , "key2":False}
+
+
+
+print(hatUeberschneidung((2,1,5,2),(0,0,4,3)))
+
+
+
 
