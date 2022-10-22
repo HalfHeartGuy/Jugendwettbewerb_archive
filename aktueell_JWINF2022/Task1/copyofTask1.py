6c9a445195eaa4017e219d4773328c3409c760f6
HalfHeartGuy <richard.h.guo@gmail.com>
2022-10-22 16:27:56 +0200

    jbwinfAUfgabe12022Runde3

--- aktueell_JWINF2022/Task1/copyofTask1.py
+++ aktueell_JWINF2022/Task1/copyofTask1.py
@@ -10,26 +10,44 @@ def checkifReim(wort1,wort2):
 
 
     #--------Wort1-------------
-    counter = 0
+    #vokale aus Wort 1
     for i in range(len(wort1) - 1,-1,-1):
+        ifskip = False
 
         for oneVokal in vokale:
             if wort1[i] == oneVokal:
                 if wort1[i - 1] == "a" or wort1[i - 1] == "A" and wort1[i] == "u":
                     vokaleListeFuerWort1.append(wort1[i - 1] + wort1[i])
+                    ifskip = True
+
                 elif wort1[i - 1] == "a" or wort1[i - 1] == "A" and wort1[i] == "i":
                     vokaleListeFuerWort1.append(wort1[i - 1] + wort1[i])
+                    ifskip = True
+
+
                 elif wort1[i - 1] == "e" or wort1[i - 1] == "E" and wort1[i] == "i":
                     vokaleListeFuerWort1.append(wort1[i - 1] + wort1[i])
+                    ifskip = True
+
+
                 elif wort1[i - 1] == "e" or wort1[i - 1] == "E" and wort1[i] == "u":
                     vokaleListeFuerWort1.append(wort1[i - 1] + wort1[i])
+                    ifskip = True
+
+
                 elif wort1[i - 1] == "ä" or wort1[i - 1] == "Ä" and wort1[i] == "u":
                     vokaleListeFuerWort1.append(wort1[i - 1] + wort1[i])
+                    ifskip = True
+
+
+
                 else:
-                    vokaleListeFuerWort1.append(wort1[i])
-
-
+                    if wort1[i + 1] != "u" and wort1[i + 1] != "i":
+                        print(wort1[i + 1])
+                        vokaleListeFuerWort1.append(wort1[i])
     print(vokaleListeFuerWort1)
+
+
 
 
 
