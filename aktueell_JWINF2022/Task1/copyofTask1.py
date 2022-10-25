1a663356ba6440a756a96a57c67cecf0d9a2c902
HalfHeartGuy <richard.h.guo@gmail.com>
2022-10-25 22:11:25 +0200

    Jugendwettbewerb_archieve Stand 25.10.2022 22:11

--- aktueell_JWINF2022/Task1/copyofTask1.py
+++ aktueell_JWINF2022/Task1/copyofTask1.py
@@ -1,8 +1,16 @@
 def checkifReim(wort1,wort2):
+    ifReim = True
     vokaleausWort1 = vokaelauseinenWort(wort1)
     vokaleausWort2 = vokaelauseinenWort(wort2)
-    return vokaleausWort1,vokaleausWort2
+    #bei doppellauten ist die Zahl dahinter um 1 erhöht also auf die zweite Buchstabe bezogen.
+    splitetdVokaleAusWort1 = []
+    for oneitem in vokaleausWort1:
+        splitetdVokaleAusWort1.append(oneitem.split(" "))
+    splitetdVokaleAusWort2 = []
 
+
+
+    return vokaleausWort1,vokaleausWort2
 
 
 
@@ -22,31 +30,31 @@ def vokaelauseinenWort(wort):
         for oneVokal in vokale:
             if wort[i] == oneVokal:
                 if wort[i - 1] == "a" or wort[i - 1] == "A" and wort[i] == "u":
-                    vokaleListeFuerWort.append(wort[i - 1] + wort[i])
+                    vokaleListeFuerWort.append(wort[i - 1] + wort[i] + " " + str(i))
 
                 elif wort[i - 1] == "a" or wort[i - 1] == "A" and wort[i] == "i":
-                    vokaleListeFuerWort.append(wort[i - 1] + wort[i])
+                    vokaleListeFuerWort.append(wort[i - 1] + wort[i] + " " + str(i))
 
                 elif wort[i - 1] == "e" or wort[i - 1] == "E" and wort[i] == "i":
-                    vokaleListeFuerWort.append(wort[i - 1] + wort[i])
+                    vokaleListeFuerWort.append(wort[i - 1] + wort[i] + " " + str(i))
 
 
                 elif wort[i - 1] == "e" or wort[i - 1] == "E" and wort[i] == "u":
-                    vokaleListeFuerWort.append(wort[i - 1] + wort[i])
+                    vokaleListeFuerWort.append(wort[i - 1] + wort[i] + " " + str(i))
 
 
                 elif wort[i - 1] == "ä" or wort[i - 1] == "Ä" and wort[i] == "u":
-                    vokaleListeFuerWort.append(wort[i - 1] + wort[i])
+                    vokaleListeFuerWort.append(wort[i - 1] + wort[i] + " " + str(i))
 
                 elif wort[i - 1] == "i" or wort[i - 1] == "I" and wort[i] == "e":
-                    vokaleListeFuerWort.append(wort[i - 1] + wort[i])
+                    vokaleListeFuerWort.append(wort[i - 1] + wort[i] + " " + str(i))
 
                 elif wort[i - 1] == "e" or wort[i - 1] == "E" and wort[i] == "e":
-                    vokaleListeFuerWort.append(wort[i - 1] + wort[i])
+                    vokaleListeFuerWort.append(wort[i - 1] + wort[i] + " " + str(i))
                 elif wort[i - 1] == "a" or wort[i - 1] == "A" and wort[i] == "a":
-                    vokaleListeFuerWort.append(wort[i - 1] + wort[i])
+                    vokaleListeFuerWort.append(wort[i - 1] + wort[i] + " " + str(i))
                 elif wort[i - 1] == "o" or wort[i - 1] == "O" and wort[i] == "o":
-                    vokaleListeFuerWort.append(wort[i - 1] + wort[i])
+                    vokaleListeFuerWort.append(wort[i - 1] + wort[i] + " " + str(i))
 
 
 
@@ -58,9 +66,9 @@ def vokaelauseinenWort(wort):
 
                     if i + 1 < len(wort):
                         if wort[i + 1] != "u" and wort[i + 1] != "i" and wort[i + 1] != "e":
-                            vokaleListeFuerWort.append(wort[i])
+                            vokaleListeFuerWort.append(wort[i] + " " + str(i))
                     if wort.endswith(oneVokal) and wort[-2] != oneVokal:
-                        vokaleListeFuerWort.append(wort[i])
+                        vokaleListeFuerWort.append(wort[i] + " " + str(i))
 
     return vokaleListeFuerWort
 
@@ -68,6 +76,6 @@ def vokaelauseinenWort(wort):
 
 
 
-wort1 = "Biene"
-wort2 = "Aas"
+wort1 = "Bankterrase"
+wort2 = "Schwank"
 print(checkifReim(wort1,wort2))
