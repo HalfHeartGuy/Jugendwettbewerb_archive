9aab5f14d294a656dedc4bd56f2c7c967c0de8aa
HalfHeartGuy <richard.h.guo@gmail.com>
2022-12-09 22:25:48 +0100

    .Jugendwettbewerb_archiv Stand:9.12.2022

--- Schach_pygame/PygameSchach.py
+++ Schach_pygame/PygameSchach.py
@@ -57,6 +57,24 @@ chess_sprite_white_tower = Figure_Chess_Visulisation(chess_board_start_x + 1/2 *
 chess_sprite_white_tower2 = Figure_Chess_Visulisation(chess_board_start_x + 7 * cell_width + 1/2 * cell_width,chess_board_start_y + 7 * cell_width + 1/2 * cell_width,"images/image_tower.png")
 chess_sprite_white_knight1 = Figure_Chess_Visulisation(chess_board_start_x + 6 * cell_width + 1/2 * cell_width,chess_board_start_y + 7 * cell_width + 1/2 * cell_width,"images/image_knight.png")
 chess_sprite_white_knight2 = Figure_Chess_Visulisation(chess_board_start_x + 1 * cell_width + 1/2 * cell_width,chess_board_start_y + 7 * cell_width + 1/2 * cell_width,"images/image_knight.png")
+chess_sprite_white_bishop1 = Figure_Chess_Visulisation(chess_board_start_x + 2 * cell_width + 1/2 * cell_width,chess_board_start_y + 7 * cell_width + 1/2 * cell_width,"images/image_bishop.png")
+chess_sprite_white_bishop2 = Figure_Chess_Visulisation(chess_board_start_x + 5 * cell_width + 1/2 * cell_width,chess_board_start_y + 7 * cell_width + 1/2 * cell_width,"images/image_bishop.png")
+chess_sprite_white_king = Figure_Chess_Visulisation(chess_board_start_x + 3 * cell_width + 1/2 * cell_width,chess_board_start_y + 7 * cell_width + 1/2 * cell_width,"images/image_king.png")
+chess_sprite_white_queen = Figure_Chess_Visulisation(chess_board_start_x + 4 * cell_width + 1/2 * cell_width,chess_board_start_y + 7 * cell_width + 1/2 * cell_width,"images/image_queen.png")
+chess_sprite_white_pawns = []
+chess_sprite_black_pawns = []
+for i in range(0,8):
+    chess_sprite_white_pawns.append(Figure_Chess_Visulisation(chess_board_start_x + i * cell_width + 1/2 * cell_width,chess_board_start_y + 6 * cell_width + 1/2 * cell_width,"images/image_pawn.png"))
+for i in range(0,8):
+    chess_sprite_black_pawns.append(Figure_Chess_Visulisation(chess_board_start_x + i * cell_width + 1/2 * cell_width,chess_board_start_y + 1 * cell_width + 1/2 * cell_width,"images/image_pawn_black.png"))
+chess_sprite_black_king = Figure_Chess_Visulisation(chess_board_start_x + 3 * cell_width + 1/2 * cell_width,chess_board_start_y + 1/2 * cell_width,"images/image_black_king.png")
+chess_sprite_black_queen = Figure_Chess_Visulisation(chess_board_start_x + 4 * cell_width + 1/2 * cell_width,chess_board_start_y + 1/2 * cell_width,"images/image_black_queen.png")
+chess_sprite_black_bishop1 = Figure_Chess_Visulisation(chess_board_start_x + 2 * cell_width + 1/2 * cell_width,chess_board_start_y + 1/2 * cell_width,"images/image_black_bishop.png")
+chess_sprite_black_bishop2 = Figure_Chess_Visulisation(chess_board_start_x + 5 * cell_width + 1/2 * cell_width,chess_board_start_y + 1/2 * cell_width,"images/image_black_bishop.png")
+chess_sprite_black_knight1 = Figure_Chess_Visulisation(chess_board_start_x + 1 * cell_width + 1/2 * cell_width,chess_board_start_y + 1/2 * cell_width,"images/image_black_knight.png")
+chess_sprite_black_knight2 = Figure_Chess_Visulisation(chess_board_start_x + 6 * cell_width + 1/2 * cell_width,chess_board_start_y + 1/2 * cell_width,"images/image_black_knight.png")
+chess_sprite_black_tower1 = Figure_Chess_Visulisation(chess_board_start_x  + 1/2 * cell_width,chess_board_start_y + 1/2 * cell_width,"images/black_image_tower.png")
+chess_sprite_black_tower2 = Figure_Chess_Visulisation(chess_board_start_x + 7 * cell_width + 1/2 * cell_width,chess_board_start_y + 1/2 * cell_width,"images/black_image_tower.png")
 
 
 
@@ -69,4 +87,24 @@ while True:
     chess_sprite_white_tower2.draw(DISPLAYSURF)
     chess_sprite_white_knight1.draw(DISPLAYSURF)
     chess_sprite_white_knight2.draw(DISPLAYSURF)
+    chess_sprite_white_bishop1.draw(DISPLAYSURF)
+    chess_sprite_white_bishop2.draw(DISPLAYSURF)
+    chess_sprite_white_king.draw(DISPLAYSURF)
+    chess_sprite_white_queen.draw(DISPLAYSURF)
+    chess_sprite_black_king.draw(DISPLAYSURF)
+    chess_sprite_black_queen.draw(DISPLAYSURF)
+    chess_sprite_black_bishop1.draw(DISPLAYSURF)
+    chess_sprite_black_bishop2.draw(DISPLAYSURF)
+    chess_sprite_black_knight1.draw(DISPLAYSURF)
+    chess_sprite_black_knight2.draw(DISPLAYSURF)
+    chess_sprite_black_tower1.draw(DISPLAYSURF)
+    chess_sprite_black_tower2.draw(DISPLAYSURF)
+
+
+
+
+    for i in chess_sprite_white_pawns:
+        i.draw(DISPLAYSURF)
+    for i in chess_sprite_black_pawns:
+        i.draw(DISPLAYSURF)
     pygame.display.update()
