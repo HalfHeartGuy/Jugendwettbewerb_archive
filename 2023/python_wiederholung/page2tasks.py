5252547fdaf6f1ec13ba34cb373a5a238aaafd07
HalfHeartGuy <richard.h.guo@gmail.com>
2023-04-01 08:25:38 +0200

    .Jugendwettbewerb_archiv Stand:16.02.2023 22:12

--- 2023/python_wiederholung/page2tasks.py
+++ 2023/python_wiederholung/page2tasks.py
@@ -17,4 +17,30 @@ def task2(counter):
                 result = str(result) + " " + str(i)
 
         print(result)
-task2(5)
+#task2(5)
+
+def task3(enternumber):
+    result = 0
+    for i in range(1,enternumber + 1):
+        result += i
+    return result
+#print(task3(4))
+
+def task4(given_number:int):
+    for i in range(1,11):
+        print(i * given_number)
+
+#task4(3)
+
+def task5(given_list:list):
+    for i in given_list:
+
+        if i > 500:
+            break
+
+        elif i > 150:
+            continue
+        else:
+            if i % 5 == 0:
+                print(i)
+task5([12, 75, 150, 180, 145, 525, 50])
