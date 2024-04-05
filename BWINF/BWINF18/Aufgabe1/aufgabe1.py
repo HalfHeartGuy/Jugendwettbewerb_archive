1bee0127c203815c46f6ddc3a526e62bb4300ca8
HalfHeartGuy <richard.h.guo@gmail.com>
2024-04-05 18:47:42 +0200

    convertion to class (nationalist code)

--- BWINF/BWINF18/Aufgabe1/aufgabe1.py
+++ BWINF/BWINF18/Aufgabe1/aufgabe1.py
@@ -1,38 +1,49 @@
 
 
+class Leiterspiel:
+    def __init__(self):
+        self.spiel_felder = [i for i in range(0,101)]
+        self.alle_Leiter = [(6,27),(14,19),(31,42),(33,38),(51,59),(53,21),(57,96),(65,85),(68,80),(70,76),(92,98)]
+        self.cur_pos = 0
+
+    #Anhand der Informationen aus alle_leiter, wird die neue Position der Spielfigur berechnet.
+    def __sprung_leiter(self, cur_pos:int) -> tuple [bool,int]:
+        for oneleiter in self.alle_Leiter:
+            if cur_pos == oneleiter[0]:
+                return (True, oneleiter[1])
+            elif cur_pos == oneleiter[1]:
+                return (True, oneleiter[0])
+        
+            
+        return False, cur_pos
+
+        
+    #Hier wird die neue Position der Spielfigur berechnet, wenn man einmal würfelt und auf ein normales Feld kommt. 
+    def __wuerfel_einmal(self,cur_pos:int, wuerfel_punkt) -> int:
+        if cur_pos + wuerfel_punkt > 100:
+            new_pos = 100 - (cur_pos + wuerfel_punkt - 100)
+        else:
+            new_pos = cur_pos + wuerfel_punkt
+        return new_pos
+    def leiterspiel_spiel_main(self,wuerfel_punkt:int)->tuple[bool,int]:
+        self.cur_pos = self.__wuerfel_einmal(self.cur_pos, wuerfel_punkt)
+        if_sprung, new_pos = self.__sprung_leiter(self.cur_pos)
+        if if_sprung:
+            self.cur_pos = new_pos
+        
+        print("cur_pos: ", self.cur_pos)
+
+
+ein_leiterspiel = Leiterspiel()
+ein_leiterspiel.leiterspiel_spiel_main(6)
+ein_leiterspiel.leiterspiel_spiel_main(6)
+
+            
 
 
 
-spiel_felder = [i for i in range(0,101)]
-alle_Leiter = [(6,27),(14,19),(31,42),(38,46),(51,59),(53,21),(57,96),(65,85),(68,80),(70,76),(92,98)]
 
 
 
 
-#Anhand der Informationen aus alle_leiter, wird die neue Position der Spielfigur berechnet.
-def sprung_leiter(cur_pos:int) -> tuple [bool,int]:
-    pass
-
-
-
-#Hier wird die neue Position der Spielfigur berechnet, wenn man einmal würfelt und auf ein normales Feld kommt. 
-def wuerfel_einmal(cur_pos:int, wuerfel_punkt) -> int:
-    pass
-    
-#folgende Funktion soll für eine angegebene Zahl zwischen 1 und 6 berechnen, 1. ob die Spielfigur auf das Zielfeld (100) erreicht,2. wie oft man würfeln muss, um das Zielfeld zu erreichen.
-#Es ist das Leiterspiel heißt es gibt Leitern, die die Figur sofort auf ein anderes Feld versetzen , aber diese können die Figur auch zurücksetzen.Das Spielbrett ist auf BWINF_37_Aufgaben_WEB.pdf zu finden bei Aufgabe 1.
-
-
-def leiterspiel_spiel_main(zahl:int)->tuple[bool,int]:
-
-
-    #wiederhole folgende Schritte mit einer while-Schleife, bis die Spielfigur das Zielfeld erreicht.
-    cur_pos = wuerfel_einmal(jetzt_pos, zahl)
-    if_sprung, new_pos = sprung_leiter(new_pos)
-    
-    if if_sprung:
-        cur_pos = new_pos
-    
-
-
-    return (True,50)
+ 
