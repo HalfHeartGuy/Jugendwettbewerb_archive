0d16c443a2ca5ebeabf54b9029d63fbff9892faa
HalfHeartGuy <richard.h.guo@gmail.com>
2024-06-13 15:01:04 +0200

    refactor: Update MongoDB queries for filtering players by country, age, and lastname

--- TestBranch/MongoDBQueries.py
+++ TestBranch/MongoDBQueries.py
@@ -5,19 +5,19 @@ germanplayers = {
 }
 #Players from china and under the age of 10
 
-({
+{
     'country': 'China', 
     'age': {
         '$lt': 10
     }
-})
+}
 
 #Players who have the lastname Guo, are older than 15 and from Germany
     
-({
+{
         'lastname': 'Guo', 
         'age': {
             '$gt': 15
         }, 
         'country': 'Germany'
-})
+}
